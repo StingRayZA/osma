d29e6fd27ed71a8e6cd6d80e01caacde33015584
Raimund Hook <1150270+StingRayZA@users.noreply.github.com>
2019-04-10 16:16:30 +0100

    db tables should create on first try now

--- dbconnect.php
+++ dbconnect.php
@@ -12,7 +12,8 @@ if (!mysqli_select_db($GLOBALS["___mysqli_ston"], 'osma')) {
 	}
 if (!mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM information_schema.tables WHERE table_schema = 'osma' AND table_name = 'data' LIMIT 1;")) {
   $sql = file_get_contents('database.sql');
-  if (!$db->query($sql)) {
+  if (!mysqli_query($GLOBALS["___mysqli_ston"], mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $sql)) {
+    echo mysqli_error($GLOBALS["___mysqli_ston"];
     die("Unable to create database tables");
   }
  } 
